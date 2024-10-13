b7777c02a1c86cd2ced579e0efee0cacb0959268
MahmoudEl3bady <melabady199@gmail.com>
2024-10-13 19:16:18 +0300

    Image form setup

---
+++ app/(root)/transforms/add/[type]/page.tsx
@@ -0,0 +1,27 @@
+import Header from "@/components/Header";
+import React from "react";
+import { transformationTypes } from "@/constants";
+import TransformsForm from "@/components/TransformsForm";
+import { auth } from "@clerk/nextjs/server";
+import { getUserById } from "@/lib/actions/user";
+import { redirect } from "next/navigation";
+
+const AddTransformationsType =  async({ params: { type } }: SearchParamProps) => {
+  const {userId} = auth();
+  if(!userId){
+    redirect("/sign-in")
+  }
+  const user = await getUserById(userId);
+  const transformation = transformationTypes[type];
+  return (
+    <>
+      <Header
+        title={transformation?.title}
+        subtitle={transformation?.subTitle}
+      />
+      <TransformsForm  action="Add" type={transformation.type as TransformationTypeKey} userId={user._id} />
+    </>
+  );
+};
+
+export default AddTransformationsType;
